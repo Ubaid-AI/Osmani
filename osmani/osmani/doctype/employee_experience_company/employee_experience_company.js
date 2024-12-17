2f01f81c553f69cfa9ffae699a5b3a24c1b6a518
Ubaid-AI <ubaidkhanzada8@gmail.com>
2024-12-17 18:07:59 +0500

    Initial commit of my Osmani app

---
+++ osmani/osmani/doctype/employee_experience_company/employee_experience_company.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ubaid Ali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Employee Experience Company", {
+// 	refresh(frm) {
+
+// 	},
+// });
