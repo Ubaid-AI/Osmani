5234cf50a8a7e5099deacf7c7bac97e2e83dc3a5
Ubaid-AI <ubaidkhanzada8@gmail.com>
2025-11-03 15:07:31 +0500

    changes

--- osmani/hooks.py
+++ osmani/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 app_include_css = "/assets/osmani/css/osmani_theme.css"
-app_include_js = "/assets/osmani/js/osmani.js"
+# app_include_js = "/assets/osmani/js/osmani.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/osmani/css/osmani.css"
