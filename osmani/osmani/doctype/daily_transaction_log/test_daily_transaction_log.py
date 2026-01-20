ff719b81664606fa396992f816bf7b210e79e698
Ubaid-AI <ubaidkhanzada8@gmail.com>
2026-01-20 12:37:37 +0500

    Dialy Transaction Log Added

---
+++ osmani/osmani/doctype/daily_transaction_log/test_daily_transaction_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Ubaid Ali and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDailyTransactionLog(FrappeTestCase):
+	pass
---
+++ osmani/osmani/doctype/transaction_log_detail/transaction_log_detail.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Ubaid Ali and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TransactionLogDetail(Document):
+	pass
